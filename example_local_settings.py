f1e2d60007f94f211f72e1200ec039e4735b2ac9
PstasDev <btndballa@outlook.hu>
2024-06-05 12:02:50 +0200

    example files

---
+++ example_local_settings.py
@@ -0,0 +1,20 @@
+SECRET_KEY = ''
+
+DEBUG = True
+
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+MELYIK = "otthon"
+
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASSWORD = "" #sajat jelszod telepitsebol
+DB_HOST = "localhost"
+
+#hagyd uresen nyugodtan
+EMAIL_HOST = ""
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = ""
+EMAIL_USE_TLS = "" # meggyozodesem hogy boolean de mindegy
+DEFAULT_FROM_EMAIL = ""
